c6fab71a6ed09f05121722526f3e0c07781eee23
Sirri262 <sirri.efe.cataloglu@akademiata.edu.pl>
2025-03-11 10:42:05 +0000

    Assign data structures to variables

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-# change it to print your name
-
-print("Hello, I am Piotr Brudny")
+# Integer
+my_int = 42
+# Floating point
+my_float = 3.14
+# String
+my_str = "Hi world"
+# List
+my_list = [1, "Python", 3.14]
+my_dict = {"name": "Sirri", "age": 20}
+my_tuple = (10, "Code", 4.5)
+my_set = {1, 2, 3, "Python"}
+my_bool = True
