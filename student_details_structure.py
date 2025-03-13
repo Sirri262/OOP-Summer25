4f368a079da404e431f7ab758f5c7b35caf731fb
Sirri262 <sirri.efe.cataloglu@akademiata.edu.pl>
2025-03-13 11:16:10 +0000

    Assign data structures to variables

---
+++ student_details_structure.py
@@ -0,0 +1,13 @@
+first_name = "Sirri"
+last_name = "Cataloglu"
+index = 34854
+nationality = "Turkey"
+starting_date = "03-09-2024"
+courses= "computer science"
+
+print (first_name)
+print (last_name )
+print (index)
+print (nationality )
+print (starting_date )
+print (courses)
