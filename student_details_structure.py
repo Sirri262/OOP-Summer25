0c8278a9dd2594f63c4f4825544d10a51e0d10af
Sirri262 <sirri.efe.cataloglu@akademiata.edu.pl>
2025-03-13 11:54:57 +0000

    Add new file

--- student_details_structure.py
+++ student_details_structure.py
@@ -1,13 +1,13 @@
-first_name = "Sirri"
-last_name = "Cataloglu"
-index = 34854
-nationality = "Turkey"
-starting_date = "03-09-2024"
-courses= "mathematice" "English" "computer science"
+student1= {'first_name':'Sirri'
+'last_name': 'Cataloglu',
+'index_number':'34854',
+'nationality': 'Turkey',
+'starting_date': '03-09-2024',
+'courses':["mathematics","English","computer science"]
 
-print (first_name)
-print (last_name )
-print (index)
-print (nationality )
-print (starting_date )
-print (courses)
+print(student1['first_name'])
+print(student1['last_name'])
+print(student1['index_number'])
+print(student1['nationality'])
+print(student1['starting_date'])
+print(student1['courses'])
