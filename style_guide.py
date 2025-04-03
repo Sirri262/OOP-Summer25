e5378e1416606f02ba566d6a6fea58f777d370e3
Sirri262 <sirri.efe.cataloglu@akademiata.edu.pl>
2025-04-03 10:57:11 +0000

    Add Person class

---
+++ style_guide.py
@@ -0,0 +1,24 @@
+VariableName = 10 #wrong
+def functionName(): #wrong
+
+variable_name = 10  #correct
+def function_name():  #correct
+
+class myclass: #wrong
+class MyClass: #correct
+
+def my_function():
+print("Hello") #wrong
+def my_function():
+    print("Hello") #correct
+
+x=10+5 #wrong
+y, z=5,10 #wrong
+x = 10 + 5 #correct
+y , z = 5 ,10 #correct
+
+def first_fuction():
+    print("First function")
+def second_fuction(): #wrong
+def first_fuction():
+    print("First function") #correct
---
+++ w3_school.py/python_variables.py
@@ -0,0 +1,22 @@
+##Variables
+x = 20
+y = "Sirri"
+print(x)
+print(y)
+
+x = 20       # x is of type int
+x = "Berke" # x is now of type str
+print(x)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+x = 20
+y = "Sirri"
+print(type(x))
+print(type(y))
+
+x = "Sirri"
+# is the same as
+x = 'Sirri'
